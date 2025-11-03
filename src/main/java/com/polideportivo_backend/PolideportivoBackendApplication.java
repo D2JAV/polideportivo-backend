d8a246e583c2e21493d6cdbe294a46b14c1f611c
Javier <javierrmujica@outlook.com.pe>
2025-11-03 18:17:58 -0500

    proyecto inicial

---
+++ src/main/java/com/polideportivo_backend/PolideportivoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.polideportivo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolideportivoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolideportivoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polideportivo_backend/PolideportivoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polideportivo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolideportivoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
